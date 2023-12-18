7f285f741fca8971a958d18e45135001c42d06ee
FrancescoM95 <mon95fran@gmail.com>
2023-12-18 17:47:28 +0100

    asked distance

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK')
+
+//* 1. Chiediamo all'utente i km che deve percorrere. 
+
+const distance = parseInt(prompt("Quanti kilometri intendi percorrere?"));
+console.log('Km:', distance);
+
+// Controllo km
+const errorMessage = "Devi inserire un numero maggiore o uguale a 1.";
+
+if (isNaN(distance) || distance < 1) {
+    alert(errorMessage);
+}
